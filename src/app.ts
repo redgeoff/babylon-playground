458e0213c4764b9368ea4e54747815163138004b
Geoff Cox <redgeoff@gmail.com>
2021-12-04 15:43:47 -0800

    fix(app): fix ts errors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import * as BABYLON from 'babylonjs';
 
-const canvas = document.getElementById("renderCanvas"); // Get the canvas element
+const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement; // Get the canvas element
 const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
 
 // Add your code here matching the playground format
@@ -10,9 +10,9 @@ const createScene = function () {
 
   BABYLON.MeshBuilder.CreateBox("box", {})
 
-  const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 15, new BABYLON.Vector3(0, 0, 0));
+  const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 15, new BABYLON.Vector3(0, 0, 0), scene);
   camera.attachControl(canvas, true);
-  const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0));
+  /*const light =*/ new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0), scene);
 
   return scene;
 };
